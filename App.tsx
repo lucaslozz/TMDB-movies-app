01eca2c815c16dce9dc704c97d7dfa1b258656cf
lucaslozz <lucass.mark@gmail.com>
2025-02-09 11:05:04 -0300

    feat: set up theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from "@shopify/restyle";
+import { View } from "react-native";
+import { theme } from "./src/theme";
+import { Box } from "./src/components/Box/Box";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Box backgroundColor="background" flex={1} />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
